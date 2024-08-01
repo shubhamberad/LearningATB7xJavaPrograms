fa34cf5fa31aa142f164541c3fd8fb6c328a857e
ShubhamBerad <shubhamberad010@gmail.com>
2024-08-01 12:30:47 +0530

    new file Lab003 added

---
+++ src/CoreJavaPrograms/Lab003.java
@@ -0,0 +1,8 @@
+package CoreJavaPrograms;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("shubham Berad");
+        System.out.println("Lab003");
+    }
+}
